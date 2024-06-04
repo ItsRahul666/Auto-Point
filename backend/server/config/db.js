e7a4c673776a9d3f2582618c399e603b2b311714
ItsRahul666 <129931633+ItsRahul666@users.noreply.github.com>
2024-06-04 14:03:09 +0530

    first commit

---
+++ backend/server/config/db.js
@@ -0,0 +1,8 @@
+let mongoose = require('mongoose')
+const dbPath = "mongodb://localhost:27017/Vehicle"
+const options = { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.connect(dbPath, options).then(res => {
+    console.log("Db Connected")
+}).catch(err => {
+    console.log("Db Connect Err", err)
+})
